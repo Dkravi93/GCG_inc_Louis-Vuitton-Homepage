9bb46145e0d4f341a1db45b0c48f8c1e10e0f7d8
Deepak Kumar Ravi <dpakravi93@gmail.com>
2025-09-21 13:19:33 +0530

    remove copy

--- gcg-inc/front/src/data/mockCollections.ts
+++
@@ -1,6 +0,0 @@
-    export const mockCollections = [
-      { id: 'summer', title: 'Summer Campaign', products: ['1', '3'] },
-      { id: 'heritage', title: 'Heritage', products: ['2'] },
-    ];
-
-
